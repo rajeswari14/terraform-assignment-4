b08afe11521b450fe125a4f700e1b60296d337f3
rajeswari14 <pavaniraji98@gmail.com>
2026-01-07 16:38:44 +0000

    Format Terraform files

--- env/prod/backend.tf
+++ env/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-task5-state"   # your S3 bucket
-    key            = "env/prod/terraform.tfstate"  # path to state file
+    key            = "env/prod/terraform.tfstate" # path to state file
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"            # your DynamoDB table
+    dynamodb_table = "terraform-locks" # your DynamoDB table
     encrypt        = true
   }
 }
--- env/prod/prod.tfvars
+++ env/prod/prod.tfvars
@@ -1,5 +1,5 @@
 # prod.tfvars
-key_name = "terraform_kp_prod"
+key_name         = "terraform_kp_prod"
 app_bucket_name  = "raji-app-bucket-prod-2026-01-05"
 data_volume_size = 20
 
